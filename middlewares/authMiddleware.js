c6fe1d1f65d6239f8e178e62d7cd4e44ff95f77e
AniketKumar5155 <your_email@example.com>
2026-01-25 12:22:10 +0530

    fix: updated authMiddleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -16,21 +16,19 @@ const authMiddleware = async (req, res, next) => {
 
         const decoded = JWT.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-        const [rows] = await pool.query(
-            `SELECT id, name, email, role
-             FROM users 
-             WHERE id = ? LIMIT 1`,
+        const result = await pool.query(
+            `SELECT id, name, email, role FROM users WHERE id = $1 LIMIT 1`,
             [decoded.userId]
         );
 
-        if (rows.length === 0) {
+        if (result.rows.length === 0) {
             return res.status(403).json({
                 success: false,
                 message: 'Access denied, Invalid user.',
             });
         }
 
-        const user = rows[0];
+        const user = result.rows[0];
 
         req.user = {
             id: user.id,
